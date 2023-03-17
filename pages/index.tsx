4506d9bd5df256496db8832beb6843c72ae5d992
maximilienk <mkru055@gmail.com>
2023-03-17 10:52:22 +0300

    services adaptive

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
                 <Features/>
                 <Containers/>
 
-                {/*<Services/>*/}
+                <Services/>
                 <Questions/>
                 <Reviews/>
                 <ContactUs/>
